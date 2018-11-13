d9aed08e73dd7a2c665be3bd00e23d39719983e8
DimitriMahon <dimitrimahon@hotmail.com>
2018-11-13 11:35:41 +0100

    Carousel update

--- bottleglass/src/components/Pages/Accueil.vue
+++ bottleglass/src/components/Pages/Accueil.vue
@@ -4,6 +4,16 @@
     <p>
 
     </p>
+
+    <carousel :per-page="1" :navigate-to="someLocalProperty" mouse-drag="false" :autoplay="true" :navigationEnabled="true">
+      <slide>
+        Slide 1 Content
+        <img src="/static/carousel/verres-pied.jpg" style="width: 100%">
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
   </div>
 </template>
 
@@ -12,6 +22,7 @@
     name: "Accueil"
   }
 </script>
+
 
 <style scoped>
 
