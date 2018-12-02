01dd56c73869edf1072f8094b10c3c87944c67a1
Clément Charmillot <clement.charmillot@divtec.ch>
2018-12-02 18:23:31 +0100

    a

--- bottleglass/dist/api/buy.php
+++ bottleglass/dist/api/buy.php
@@ -4,7 +4,7 @@
 	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 	header('Content-type: text/html; charset=UTF-8'); 
 	header('Access-Control-Allow-Headers: X-Requested-With');
-	
+	/*
 	$name		= $_POST['name'];
 	$forname 	= $_POST['forname'];
 	$gender		= $_POST['gender'];
@@ -13,16 +13,37 @@
 	$address 	= $_POST['address'];
 	$country 	= $_POST['country'];
 	$email 		= $_POST['email'];
+	*/
+	$name		= $_GET['name'];
+	$forname 	= $_GET['forname'];
+	$gender		= $_GET['gender'];
+	$npa 		= $_GET['npa'];
+	$locality 	= $_GET['locality'];
+	$address 	= $_GET['address'];
+	$country 	= $_GET['country'];
+	$email 		= $_GET['email'];
 	
 	$title = "Bottleglass - Commande";
 	
-	//$headers  = "MIME-Version: 1.0\r\n";
+	$headers  = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$headers .= 'content-transfer-encoding: quoted-printable\r\n';
+	$headers .= 'content-type: text/plain; charset="UTF-8"';
+	$headers .= 'Mime-Version: 1.0';
 	
-	$content = "Les commandes ne sont pas encore fonctionnelles. Aucune commande n'a encore réellement été faite. Veuillez réessayer plus tard\n" .
-					file_get_contents("./mailTemplate.html");
+	$mail = file_get_contents("./mailTemplate.html");
 	
-	mail($email, $title, $content, $headers);
+	$mail = str_replace('%%NAME%%'	 	, $name		, $mail);
+	$mail = str_replace('%%FORNAME%%'	, $forname	, $mail);
+	
+	$content = $mail;	
+	
+	if (mail($email, $title, $content, $headers)) {
+		echo $content;
+	}
+	
+	else {
+		echo "Error sending mail.";
+	}
 
-	echo $content;
 
