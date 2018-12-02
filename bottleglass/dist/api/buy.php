3a9d2ca7a0e5d1ba857b6e8e38f7bc871c8c165d
Clément Charmillot <clement.charmillot@divtec.ch>
2018-12-02 16:39:03 +0100

    abcd

--- bottleglass/dist/api/buy.php
+++ bottleglass/dist/api/buy.php
@@ -5,9 +5,24 @@
 	header('Content-type: text/html; charset=UTF-8'); 
 	header('Access-Control-Allow-Headers: X-Requested-With');
 	
-	$mail = $_POST['email'];
+	$name		= $_POST['name'];
+	$forname 	= $_POST['forname'];
+	$gender		= $_POST['gender'];
+	$npa 		= $_POST['npa'];
+	$locality 	= $_POST['locality'];
+	$address 	= $_POST['address'];
+	$country 	= $_POST['country'];
+	$email 		= $_POST['email'];
 	
-	mail($mail, "Bottleglass - Commande", "Test");
+	$title = "Bottleglass - Commande";
+	
+	//$headers  = "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	
+	$content = "Les commandes ne sont pas encore fonctionnelles. Aucune commande n'a encore réellement été faite. Veuillez réessayer plus tard\n" .
+					file_get_contents("./mailTemplate.html");
+	
+	mail($email, $title, $content, $headers);
 
-	echo "{r:1}";
+	echo $content;
 
