411e81084ce83668cfd2faa5f1343148ab85829b
DimitriMahon <dimitrimahon@hotmail.com>
2018-12-11 10:09:13 +0100

    Cross Domain GetProducts

--- bottleglass/dist/api/getProducts.php
+++ bottleglass/dist/api/getProducts.php
@@ -1,11 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dimitri
- * Date: 04.12.2018
- * Time: 11:09
- */
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Content-type: text/html; charset=UTF-8');
+header('Access-Control-Allow-Headers: X-Requested-With');
 try {
     $db = new PDO('mysql:host=xy1fd.myd.infomaniak.com;dbname=xy1fd_bottleglass', 'xy1fd_admin','XUxtMTgfTqcx');
 
