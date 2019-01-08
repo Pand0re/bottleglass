3dc5ad05d1da4ae6a774f5d31145277139d731a8
DimitriMahon <dimitrimahon@hotmail.com>
2019-01-08 11:37:35 +0100

    Mail

--- bottleglass/dist/api/buy.php
+++ bottleglass/dist/api/buy.php
@@ -20,7 +20,7 @@
 	$title = "Bottleglass - Commande";
 	
 	$headers  = "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8-1\r\n";
 	$headers .= 'content-transfer-encoding: quoted-printable\r\n';
 	$headers .= 'content-type: text/plain; charset="UTF-8"';
 	$headers .= 'Mime-Version: 1.0';
