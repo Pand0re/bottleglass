985247671bafb74ed7ba004a39f20d6afc8c4fa6
Clément Charmillot <clement.charmillot@divtec.ch>
2019-01-08 10:48:39 +0100

    kk

--- bottleglass/src/components/Footer.vue
+++ bottleglass/src/components/Footer.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="container">
+    <p ref="lang" v-html="$t('t.miscellaneous.lang')"></p>
     &copy; 2019 Bottle Glass. {{ $t('t.miscellaneous.AllRightsReserved') }}
   </div>
 </template>
 
 <script>
   export default {
-    name: "Footer"
+    name: "Footer",
+
+    mounted: function() {
+
+      const Self = this;
+      const LangChange = this.$refs.lang;
+
+      const fr = LangChange.querySelector('#fr');
+      const de = LangChange.querySelector('#de');
+      const en = LangChange.querySelector('#en');
+
+      fr.addEventListener('click', function () {
+        Self.changeLang('fr');
+      });
+      de.addEventListener('click', function () {
+        Self.changeLang('de');
+      });
+      en.addEventListener('click', function () {
+        Self.changeLang('en');
+      });
+    },
+
+    methods: {
+      changeLang(lang) {
+        this.$i18n.locale = lang;
+      }
+    }
   }
 </script>
 
@@ -18,5 +45,19 @@
     /*background: #f3e5ab;*/
     font-size: 0.8em;
     text-align: center;
+  }
+
+  p {
+    text-align: center;
+  }
+
+  p >>> span {
+    color: #777;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  p >>> span:hover {
+    color: black;
   }
 </style>
