48880cea7e212e0ac57d6fe9fe06bbcd23fb2f5a
DimitriMahon <dimitrimahon@hotmail.com>
2019-01-15 10:23:26 +0100

    yo

--- bottleglass/dist/api/buy.php
+++ bottleglass/dist/api/buy.php
@@ -101,7 +101,7 @@
     $mail = str_replace('%%ORDERNUMBER%%'	, PK_COMM 		, $mail);
 
 
-	$content = htmlentities($mail);
+	$content = $mail;
 	
 	// Send mail
 	if (mail($email, $title, $content, $headers)) {
